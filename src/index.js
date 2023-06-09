8e409f32590bc889a6eb3597631d14fb3a7ff498
Himanshu <himanshubarde2001@gmail.com>
2023-06-09 23:57:43 +0530

    Basic setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const connect = require("./config/database");
+
+app.listen(3000, async () => {
+  console.log("Server started");
+  await connect();
+  console.log("Mongo db connected");
+});
