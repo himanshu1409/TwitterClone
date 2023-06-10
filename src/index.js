547151722f46090b25e40efc284f379a4a56b389
Himanshu <himanshubarde2001@gmail.com>
2023-06-10 14:57:45 +0530

    Added README

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ const express = require("express");
 const app = express();
 
 const connect = require("./config/database");
-const TweetRepository = require("./repository/tweet-repository");
-const Comment = require("./models/comment");
 
 app.listen(3000, async () => {
   console.log("Server started");
   await connect();
   console.log("Mongo db connected");
-
-  const tweetRepo = new TweetRepository();
 });
